dc8e393b4ac00a29e1db6b708822a1e71180235c
Gabriel Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2024-11-17 16:13:01 -0300

    EX002-SalarioComBonus

---
+++ EX002-SalarioComBonus.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    double slr,vendas;
+    char nome[100];
+
+    printf("Digite o nome do vendedor: ");
+    scanf("%s", &nome);
+
+    printf("Digite o salario do vendedor: ");
+    scanf("%lf", &slr);
+
+    printf("Digite o total arrecado em vendas feitas pelo vendedor: ");
+    scanf("%lf", &vendas);
+
+    vendas = vendas * 0.15;
+    slr = slr + vendas;
+    
+    printf("Vendedor: %s\nComissao: R$%.2lf\nSalario: R$%.2lf", nome, vendas, slr);
+}
